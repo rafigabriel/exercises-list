7a0a1e2424ab579a95ae198e47833d0e26cb0e29
Rafi Gabriel <rafigabriel.32@gmail.com>
2024-08-08 00:12:45 -0300

    Question 04 is still being finished.

--- src/App.java
+++ src/App.java
@@ -3,43 +3,49 @@ public class App {
 
         // QUESTION 1:
         
-            /*List1 list1 = new List1();
-            int[] elements = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            List1 list1 = new List1();
+            int[] elements = { 12, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
             for (int i = 0; i < elements.length; i++) {
-                List1.insert(elements[List2
+                list1.insert(elements[i]);
+            }
             System.out.println("First list created:");
             System.out.println("-----------------------------------------------");
-            List1.print();
+            list1.print();
             System.out.println("-----------------------------------------------");
-            List1.remove();
-            List1.remove();
+            list1.remove();
+            list1.remove();
             System.out.println("First list after some removals:");
             System.out.println("-----------------------------------------------");
-            List1.print();*/
+            list1.print();
 
          
         // --------------------------------------------------------------------------
 
         // QUESTION 2:
 
-            /*List2 list2 = new List2();
-            List2.insertOrdered(10);
-            List2.insertOrdered(45);
-            List2.insertOrdered(366);
-            List2.insertOrdered(988);
-            List2.insertOrdered(444);
-            List2.insertOrdered(22);
+            List2 list2 = new List2();
+            list2.insertOrdered(10);
+            list2.insertOrdered(45);
+            list2.insertOrdered(366);
+            list2.insertOrdered(988);
+            list2.insertOrdered(444);
+            list2.insertOrdered(22);
             System.out.println("-----------------------------------------------");
             System.out.println("Second list created and the elements were inserted neatly:");
             System.out.println("-----------------------------------------------");
-            List2.print();*/
+            list2.print();
         //--------------------------------------------------------------------------
 
         // QUESTION 3: It needs the two questions above to work.
-            /*List2.merge(list1);
+            /*list2.merge(list1);
             System.out.println("--------------------------------------------------");
             System.out.println("Joined lists:");
-            List2.print();*/
-        //--------------------------------------------------------------------------
+            list2.print();*/
+            //--------------------------------------------------------------------------
+            list2.mergeOrdered(list1);
+            System.out.println("Lists merged neatly : ");
+            list2.print();
+
+        
     }
 }
--- src/List2.java
+++ src/List2.java
@@ -71,7 +71,7 @@ public class List2 {
 
     // QUESTION 3: This method merges the First List (List1.java) and the Second List (List2.java).
     public void merge(List1 list1) {
-        if (firstCell == null) {
+        if (empty()) {
             firstCell = list1.firstCell;
         } else {
             Cell aux = firstCell;
@@ -81,4 +81,66 @@ public class List2 {
             aux.next = list1.firstCell;
         }
     }
+
+    public void mergeOrdered(List1 list1) {
+        if (empty()) {
+            firstCell = list1.firstCell;
+        }
+        else {
+            Cell aux = firstCell;
+            Cell aux2 = firstCell.next;
+            if(aux2 == null){
+                if(aux.value < list1.firstCell.value){
+                    insertAtEnding(list1.firstCell.value);
+                }
+                else{
+                    insertAtBeginning(list1.firstCell.value);
+                }
+            }
+            else{
+                while (aux2.next != null && aux2.value < list1.firstCell.value) {
+                    aux = aux.next;
+                    aux2 = aux2.next;
+                }
+                if(aux2.next == null && aux.value < list1.firstCell.value){
+                    insertAtEnding(list1.firstCell.value);
+                }
+                else{
+                    aux.next = list1.firstCell;
+                    list1.firstCell.next = aux2;
+                }
+            }
+        }
+    }
+
+    /*
+     * public void insertOrdered(int value) {
+     * Cell elementCell = new Cell(value);
+     * 
+     * if (empty()) {
+     * firstCell = elementCell;
+     * } else {
+     * Cell aux = firstCell;
+     * Cell aux2 = firstCell.next;
+     * if (aux2 == null) {
+     * if (aux.value < elementCell.value) {
+     * insertAtEnding(value);
+     * } else {
+     * insertAtBeginning(value);
+     * }
+     * } else {
+     * while (aux2.next != null && aux2.value < elementCell.value) {
+     * aux = aux.next;
+     * aux2 = aux2.next;
+     * }
+     * if (aux2.next == null && aux2.value < elementCell.value) {
+     * insertAtEnding(value);
+     * } else {
+     * aux.next = elementCell;
+     * elementCell.next = aux2;
+     * }
+     * }
+     * }
+     * }
+     */
 }
